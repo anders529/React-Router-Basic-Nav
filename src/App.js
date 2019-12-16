2f6a57abf2603585bd0db7658802258e2de0e896
Aaron Andersen <anderseana@tutanota.com>
2019-12-16 16:49:56 -0600

    imported everything

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter as Route} from 'react-router-dom';
+import {Home, About, Navigation} from './components';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
 
